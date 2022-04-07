8a1c5048de12120d50ac465ac79c17f10be50a48
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-04-07 16:40:58 +0530

    Time: 3 ms (74.50%), Space: 8.3 MB (79.14%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> result(n, 0);
+        if( k == 0 ) return result;
+        
+        else if( k > 0 ){
+            for( int index = 0 ; index<n; index++ ){
+                int i = 1;
+                int temp = 0;
+                while( i <= k ){
+                    temp += code[(index+i)%n];
+                    i++;
+                }
+                result[index] = temp;
+            }
+        }
+        else if( k < 0 ){
+            for( int index = n-1; index >= 0 ; index-- ){
+                int i = 1;
+                int temp = 0;
+                while( i <= (-k)){
+                    int j = index-i;
+                    if( j < 0 ) j+=n;
+                    temp +=code[j];
+                    i++;
+                }
+                result[index] = temp;
+            }
+        }
+        return result;
+    }
+};    
