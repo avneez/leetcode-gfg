1b170f86dc7ce81ea443031f3cac2f217198051e
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-04-08 21:00:11 +0530

    Time: 7 ms (56.94%), Space: 11.2 MB (73.76%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkRotation(vector<vector<int>>& mat, vector<vector<int>>& target){
+        int n = mat.size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if (mat[i][j]!=target[i][j]) return false; 
+            }
+        }
+    return true;
+}
+
+    void Rotation90(vector<vector<int>>&mat){
+        int n = mat.size();
+        reverse(mat.begin(), mat.end());
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+    }
+    
+     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for(int i = 0;i<4; i++){
+            Rotation90(mat);
+            if(checkRotation(mat, target)) return true;
+        }
+        return false;       
+    }
+};
