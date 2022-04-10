c8a161d3f8c625f406fd4812c9b628ab6b9b6b3a
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-04-10 19:38:51 +0530

    Time: 7 ms (43.28%), Space: 8.3 MB (69.92%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -3,24 +3,28 @@ public:
     int calPoints(vector<string>& ops) {
         int ans=0;
         stack<int> st;
-        unordered_map<string, int> mp={{"C",1}, {"D",2}, {"+",3}};
+        // unordered_map<string, int> mp={{"C",1}, {"D",2}, {"+",3}};
         
         for (int i=0; i<ops.size(); i++){
-            if(mp[ops[i]]!=1 && mp[ops[i]]!=2 && mp[ops[i]]!=3){
-               int x = stoi(ops[i]);
-                st.push(x);
-            }  
+            // if(mp[ops[i]]!=1 && mp[ops[i]]!=2 && mp[ops[i]]!=3){
+            //    int x = stoi(ops[i]);
+            //     st.push(x);
+            // }  
             if(ops[i]=="C")
                     st.pop();
-            if(ops[i]=="D")
+            else if(ops[i]=="D")
                     st.push(st.top()*2);
-            if(ops[i]=="+")
+            else if(ops[i]=="+")
             {
                 int temp=st.top();
                 st.pop();
                 int a=temp+st.top();
                 st.push(temp);
                 st.push(a);
+            }
+            else
+            {
+                st.push(stoi(ops[i]));
             }
                     
         }
