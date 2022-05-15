33cd19fd4e0801e95a6b85e03419a818dbe0228f
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-05-16 02:44:01 +0530

    Time: 79 ms (45.13%), Space: 31.8 MB (59.61%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> ans;
+    
+    //using MaxHeap
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         vector<int> ans;
         
-        priority_queue<pair<int,int>> maxh;
-        for(int i=0; i<arr.size(); i++){
-            maxh.push({abs(arr[i]-x), arr[i]});
-            if(maxh.size()>k) maxh.pop();
+//         priority_queue<pair<int,int>> maxh;
+//         for(int i=0; i<arr.size(); i++){
+//             maxh.push({abs(arr[i]-x), arr[i]});
+//             if(maxh.size()>k) maxh.pop();
+//         }
+//         while(maxh.size()>0){
+//             int x = maxh.top().second;
+//             ans.push_back(x);
+//             maxh.pop();
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+    
+    //binary search approach
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l=0,r =arr.size()-k;
+        vector<int>ans;
+        while(l<r){
+            int mid =(l+r)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                l=mid+1;
+            }
+            else{
+                r=mid; 
+            }   
         }
-        while(maxh.size()>0){
-            int x = maxh.top().second;
-            ans.push_back(x);
-            maxh.pop();
+        for(int i=l;i<l+k;i++){
+            ans.push_back(arr[i]);
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
+
