09ca644496711d179f8f6dda48adaa07d5aeb39b
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-06-09 00:00:44 +0530

    Added solution - LeetHub

--- Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
+++ Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
@@ -5,12 +5,34 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2)
-    {
-        sort(A,A+N);
-        long long sum =0;
-        for(int i = K1; i<K2-1; i++)
-           sum += A[i];
+    // long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2){
+    //     sort(A,A+N);
+    //     long long sum =0;
+    //     for(int i = K1; i<K2-1; i++)
+    //       sum += A[i];
+    //   return sum;
+    
+    //Using maxHeap
+    long long kthElement(long long A[], long long N, long long K){
+       priority_queue<int> pq;
+       for(int i = 0 ; i < N ; i++){
+           pq.push(A[i]);
+           if(pq.size() > K) pq.pop();
+       }
+       return pq.top();
+   }
+   
+   
+   long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2){
+       // Your code goes here
+       long long first = kthElement(A, N, K1);
+       long long second = kthElement(A, N, K2);
+       long long sum = 0;
+       for(int i = 0 ; i < N ; i++){
+           if(A[i] > first && A[i] < second){
+               sum += A[i];
+           }
+       }
        return sum;
     }
 };
