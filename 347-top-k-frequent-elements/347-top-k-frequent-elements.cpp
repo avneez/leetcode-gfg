1be08335b743fcaa9c8a2b79cc9a0bb9cd46dbc9
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-06-08 16:41:29 +0530

    Time: 19 ms (68.21%), Space: 13.7 MB (42.80%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -9,8 +9,8 @@ public:
         }
         
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minh;
-        for(auto i=mp.begin(); i!=mp.end(); i++){
-            minh.push({i->second, i->first});
+        for(auto i:mp){
+            minh.push({i.second, i.first});
             if (minh.size()>k) minh.pop();
         }
         while(minh.size()>0){
