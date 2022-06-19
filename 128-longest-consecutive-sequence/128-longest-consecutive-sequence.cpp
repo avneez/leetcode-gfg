afd2f47558951251a8b576c8a9bfb4ea4f73c77e
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-06-20 03:50:32 +0530

    Time: 2612 ms (5.00%), Space: 51.1 MB (20.18%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,16 +4,19 @@ public:
              
         if(nums.size()==0) return 0;
         
-        int max_res=1;
-        unordered_set<int> m;
-        
-        for(int i=0;i<nums.size();i++)  m.insert(nums[i]); 
-            
+        set<int> s;
+        for(int i=0;i<nums.size();i++)  s.insert(nums[i]); 
+         
+        int max_res=0;
         for(int i=0;i<nums.size();i++) {
-            if(m.count(nums[i]-1))  continue;   
-            int res = 1;
-            while(m.count(nums[i]+res)) res++;  
-            max_res = max(res,max_res);
+            if(!s.count(nums[i]-1)) { 
+                int curnum = nums[i];
+                int res = 1;
+                while(s.count(curnum+1)){
+                    curnum++; res++;
+                } 
+                max_res = max(res,max_res);
+            }
         }
         return max_res;
     }
