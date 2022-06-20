7b9ed9ee35d3bcc04005ea6b320653b076d4aad3
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-06-20 23:08:31 +0530

    Time: 174 ms (26.74%), Space: 18.3 MB (69.31%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    // Compare function for the condition of the sorting i.e. according to decreasing length 
-    static bool compare (string& first, string& second) {
-        return first.size() > second.size();
-    }
     int minimumLengthEncoding(vector<string>& words) {
-        unordered_map<string, int> mp;
-        int ans = 0;
+        set<string> s(words.begin(), words.end());     
         
-        //sorting the array in decreasing length sequence
-        sort(words.begin(), words.end(), compare);
-
-        //Entering each word in hash map.
-        for(int i=0; i<words.size(); i++){
-            mp[words[i]]++;
+        for(auto it:s){
+            int n=it.size();
+            for(int i=1; i<n; i++){
+                string str = it.substr(i,n);
+                
+                if(s.find(str)!=s.end())
+                    s.erase(str);
+            }
         }
-        
-        //For each word if it exist in hash map then add its length+1 to ans as # is also included.
-        for(int i=0; i<words.size(); i++){
-            int k = words[i].size();
-            if(mp[words[i]] >0){
-                ans += (k+1);
-            }
-            
-            //Deleting record of any suffix of the word from hash map as it is already covered.
-            for(int j=k-1; j>=0; j--){
-                string s = words[i].substr(j, k-j);
-                mp[s] = 0;
-            }
+        int ans=0;
+        for(auto it:s){
+            ans+=it.size()+1;
         }
         return ans;
     }
