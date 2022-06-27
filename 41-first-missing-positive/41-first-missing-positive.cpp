4a0bab2b6533c4f00fa3c262f423b45876b88594
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-06-27 16:50:16 +0530

    Time: 235 ms (54.21%), Space: 85 MB (24.90%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,19 +2,37 @@ class Solution{
 public:
 
     int firstMissingPositive(vector<int>& nums) {
-        int curr = 0, n = nums.size(); // take 2 pointers
-        while( curr < n){ // iterating through nums array
-            // if indexed el is >0 && <=n && not equal to index
-            if(nums[curr] > 0 && nums[curr] <= n && nums[curr] != nums[nums[curr] - 1])
-                swap(nums[curr],nums[nums[curr] - 1]); // swap them to change into matching
-            else
-                curr++;
+        
+        //cyclic sort
+        
+        // int curr = 0, n = nums.size(); // take 2 pointers
+        // while( curr < n){ // iterating through nums array
+        //     // if indexed el is >0 && <=n && not equal to index
+        //     if(nums[curr] > 0 && nums[curr] <= n && nums[curr] != nums[nums[curr] - 1])
+        //         swap(nums[curr],nums[nums[curr] - 1]); // swap them to change into matching
+        //     else
+        //         curr++;
+        // }
+        // for(int i = 0;i<n;i++){
+        //     if(nums[i] != i + 1){
+        //         return i + 1;
+        //     }
+        // }
+        // return n + 1;
+        
+        // using freq map arr
+        int mp[500001] = {0}; //declaring map array of constant size as S=O(1)
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if (nums[i] <= n && nums[i] > 0)
+                mp[nums[i]]=1;
         }
-        for(int i = 0;i<n;i++){
-            if(nums[i] != i + 1){
-                return i + 1;
-            }
+        int pos=0;
+        int i=1;
+        while(pos<n){
+            if(mp[i]==0) return i;
+            pos++; i++;
         }
-        return n + 1;
+        return i;      
     }
 };
