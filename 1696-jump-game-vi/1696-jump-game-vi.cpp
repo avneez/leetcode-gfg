4edd1533eab918eecdde22e23f7c1a15b64b5c87
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-10 01:41:09 +0530

    Time: 178 ms (97.47%), Space: 77.2 MB (99.06%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> dp(n);
-        dp[n-1]=nums[n-1];
+//         int n = nums.size();
+//         vector<int> dp(n);
+//         dp[n-1]=nums[n-1];
         
-        priority_queue<pair<int,int>> maxh;
-        maxh.push({dp[n-1],n-1});
+//         priority_queue<pair<int,int>> maxh;
+//         maxh.push({dp[n-1],n-1});
         
-        for(int i=n-2; i>=0; i--){
-            while(maxh.size() and maxh.top().second>i+k){
-                maxh.pop();
-            }
-                dp[i]= maxh.top().first+nums[i];
-                maxh.push({dp[i],i});
+//         for(int i=n-2; i>=0; i--){
+//             while(maxh.size() and maxh.top().second>i+k){
+//                 maxh.pop();
+//             }
+//                 dp[i]= maxh.top().first+nums[i];
+//                 maxh.push({dp[i],i});
+//         }
+//         return dp[0];
+//     }
+        
+        int n=nums.size();
+        deque<int> d{0};
+        
+        for(int i=1; i<n; i++){
+            if(d.front()+k<i) 
+                d.pop_front();
+            nums[i] += nums[d.front()];
+            while(!d.empty() and nums[d.back()]<=nums[i])
+                d.pop_back();
+            d.push_back(i);
         }
-        return dp[0];
+        return nums.back();
     }
 };
