ca2c333d98f681232e84ddfaa96f11e7cf189e81
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-10 10:22:44 +0530

    Time: 3 ms (100.00%), Space: 11.8 MB (100.00%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -2,29 +2,12 @@ class Solution {
 public:
     int fillCups(vector<int>& amount) {
         int sec =0;
-
-        priority_queue<int> q;
-        for(auto i:amount) q.push(i);
-        
-        while(q.top()!=0){
-            int a=q.top(); q.pop();
-            int b=q.top(); q.pop();
-            int c=q.top(); q.pop();
-            
-            if(b!=0){
-                sec++;
-                b-=1;
-                a-=1;
-                q.push(b);
-                q.push(a);
-                q.push(c);
-            }
-            else{
-                sec+=a;
-                a=0;
-            }           
+        sort(amount.begin(), amount.end());
+        if(amount[0]+amount[1]>amount[2]){
+            int s= amount[0]+amount[1]+amount[2];
+           return s/2+s%2;
         }
-       return sec;
+        else return amount[2];
     }
         
         // int mx = 0, sum = 0;
