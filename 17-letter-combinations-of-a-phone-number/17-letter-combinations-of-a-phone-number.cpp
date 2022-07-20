991e03830ac7ec85977c89e30dd8f1092d125550
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-21 02:37:15 +0530

    Time: 6 ms (14.06%), Space: 7 MB (10.10%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void fun(string digits,vector<string> str,vector<string> &ans,int n,int i,string temp){
-        if(i>n)
-            return ;
-        if(i==n){
+        // if(i>n) return ;
+        if(i>=n){
             ans.push_back(temp);
             return ;
          }
         for(char s : str[(digits[i]-'0')])
             fun(digits,str,ans,n,i+1,temp+s);
+            temp.pop_back();  //backtrack without this line also running
         }
     vector<string> letterCombinations(string digits) {
         if(digits == "")
