faf87b116e175200ecd445c1623095ce5c72e475
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-20 18:16:43 +0530

    Time: 20 ms (87.84%), Space: 19.5 MB (92.00%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        // sort(nums.begin(), nums.end());
+        // return nums[nums.size()/2];
+        
+        int c=0;
+        int ans=-1;
+        
+        for(int num:nums){
+            if(c==0) ans=num;
+            if(num==ans) c+=1;
+            else c-=1;
+        }
+        return ans;
      }
 };
