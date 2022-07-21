ba83d820dc1f561ce104ca3863c816fd58afd2fa
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-21 17:20:12 +0530

    Time: 11 ms (13.12%), Space: 16.1 MB (18.77%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -5,12 +5,12 @@ public:
         if(i==nums.size()){
             ans.push_back(subs);
             return;
-        }
-        generate(nums,i+1,subs);
+        } 
+        generate(nums,i+1,subs);    //don't pick
         
-        subs.push_back(nums[i]);
+        subs.push_back(nums[i]);    //pick and call for next
         generate(nums,i+1,subs);
-        subs.pop_back();
+        subs.pop_back();            //backtrack
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
