53760d366d26f3c05c9c8af39138b70cd5a6069c
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-22 23:44:57 +0530

    Time: 4 ms (87.24%), Space: 10.2 MB (73.43%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -12,8 +12,8 @@ class Solution {
 public:
      ListNode* partition(ListNode* head, int x) {
         
-        ListNode* small = new ListNode(-1);
-        ListNode* large = new ListNode(-1);
+        ListNode* small = new ListNode(0);
+        ListNode* large = new ListNode(0);
         ListNode* small_head = small;
         ListNode* large_head = large;
         
