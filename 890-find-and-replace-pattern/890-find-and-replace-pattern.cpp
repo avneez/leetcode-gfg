78f578a0e57f3563c8e01765796252849c420243
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-07-30 02:38:52 +0530

    Time: 12 ms (19.17%), Space: 8.5 MB (43.89%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-    
-    vector<int> found_Pattern(string pattern)
-    {
-	   // if string is empty return empty vector.
-        if(pattern.size() == 0)
-            return {};
-        
+    vector<int> found_pattern(string pattern){
         vector<int> v;
-		
-		// ind variable for keeping track of unique characters
-        int ind = 0;
-		
+        int ind=0;
+        
         unordered_map<char,int> mp;
-        for(int i = 0; i<pattern.size(); ++i)
-        {
-			// if character not present in map, insert the char with an index,
-			// increment index because for next unique character the index should be differnt.
-           if(mp.find(pattern[i]) == mp.end())
-           {
-               mp.insert({pattern[i],ind++});
-			   // also  push the index to v(numeric  pattern vector)
-			   // mp[pattern[i]] gives the key to that character, here key is ind(which we are giving to every unique character)
-               v.push_back(mp[pattern[i]]);
-           }
-            else
-            {
-				// if char is already in map than push index mapped to that character into the vector
+        for(int i=0; i<pattern.size(); i++){
+            if(mp.find(pattern[i])==mp.end()){
+                mp.insert({pattern[i],ind++});
+                v.push_back(mp[pattern[i]]);
+            }
+            else{
                 v.push_back(mp[pattern[i]]);
             }
         }
-		// return numeric pattern
         return v;
     }
+    
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-       
-		// store numeric patten of Pattern string in v
-        vector<int> v = found_Pattern(pattern);
-        
-        int n = words.size();
         vector<string> ans;
+        vector<int> v2 = found_pattern(pattern); 
         
-		// iterating and comparing pattern of each word with v(numeric pattern of Pattern)
-        for(int i = 0; i<n; ++i)
-        {
-            vector<int> pattern_word = found_Pattern(words[i]);
-            
-			// if matched add words[i] to answer vector
-            if(v == pattern_word)
+        for(int i=0; i<words.size(); i++){
+            vector<int> word_pattern = found_pattern(words[i]);
+            if(v2==word_pattern){
                 ans.push_back(words[i]);
+            }
         }
-        
         return ans;
     }
 };
