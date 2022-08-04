ae4e17b5f3d42c8a42fbd63d7935f7a29b053adc
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-08-04 23:35:26 +0530

    Time: 821 ms (15.98%), Space: 48.2 MB (25.06%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,90 +1,38 @@
-class Solution
-{
+class Solution {
 public:
-    int dx[4] = {0, 0, -1, 1};
-    int dy[4] = {1, -1, 0, 0};
-    // we have defined the change in coordinate if they move in specific directions 
-
-    bool isvalid(int x, int y, int n, int mid)
-    {
-        return (x >= 0 && x < n && y >= 0 && y < mid);
-        // to check whether on which path we are moving is valid or not 
-    }
-
-
-    bool bfs(vector<vector<int>> heights, int k)
-    {
-        queue<pair<int, int>> q;
-        // queue to perform the BFS logic 
-        q.push({0, 0});
-        // initially push the first cell of matrix 
-        vector<vector<bool>> visited(101, vector<bool>(101, false));
-        // make a visisted array initially with all the value false initially 
-        visited[0][0] = 1;
-        // since it visited , makr it a true  
-
-        while (!q.empty())
-        {
-            pair<int, int> curr = q.front();
-            // pick out the front 
-            q.pop();
-            int cx = curr.first;
-            int cy = curr.second;
-            // get the current x and y 
-            if (cx == heights.size() - 1 && cy == heights[0].size() - 1)
-                return true;
-            // if we are reached the last index we are done
-            // else just go in all 4 direction and check conditions  
-            for (int i = 0; i < 4; i++)
-            {
-                int x = dx[i] + cx;
-                int y = dy[i] + cy;
-                // each time make new x , y 
-                if (isvalid(x, y, heights.size(), heights[0].size()) && !visited[x][y])
-                {
-                    // if that x,y is valid , just check tah absolute difference is less tha tha mid or not 
-                    if (abs(heights[cx][cy] - heights[x][y]) <= k)
-                    {
-                        visited[x][y] = true;
-                        // if yes visit that node 
-                        q.push({x, y});
-                        // push it in queue 
-                    }
-                }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m=heights.size();
+        int n=heights[0].size();
+        
+        vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
+        int ans=0;
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minh;
+        //bfs
+        minh.push({0,0,0});
+        while(minh.size()){
+            auto res=minh.top();
+            minh.pop();
+            ans=max(ans,res[0]); //update ans
+            
+            int i=res[1]; int j=res[2];
+            //conditions
+            if(heights[i][j]==-1)  //already visited
+                continue;
+            if(i==m-1 and j==n-1)  //reached last cell
+                break;
+            
+            //checking for new directions
+            for(int d=0; d<4; d++){
+                int newi = i+dir[d][0];
+                int newj = j+dir[d][1];
+                if(newi<0 || newj<0 || newi==m || newj==n || heights[newi][newj]==-1)
+                    continue;
+                
+                minh.push({abs(heights[i][j]-heights[newi][newj]),newi,newj}); //push effort and posn
             }
-        }
-        return false;
-    }
-
-    int minimumEffortPath(vector<vector<int>> &heights)
-    {
-        // we have similar binary search template  
-
-        int low = 0;
-        int high = 1e6;
-        int ans = -1;
-
-        // set high and low  
-
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            // get a mid 
-            // apply bfs over that mid value 
-            if (bfs(heights, mid))
-            {
-                // if that bfs on that value return true , it might be our answer , so store it 
-                ans = mid;
-                high = mid - 1;
-                // since we want want min value , high=mid-1
-            }
-            else
-            {
-                low = mid + 1;
-                // else increase the value and check for it 
-            }
+            heights[i][j]=-1;
         }
         return ans;
-        //  return the ans 
     }
 };
