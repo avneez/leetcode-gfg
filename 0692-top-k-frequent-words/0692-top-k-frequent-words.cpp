6fc4d540ca8697f67dc287c5381679198ae17860
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-10-19 12:26:12 +0530

    Time: 39 ms (9.76%), Space: 12.7 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool static compare(pair<int,string> p1, pair<int,string> p2){
-        if(p1.first==p2.first) return p2.second>p1.second;
+        if(p1.first==p2.first) return p1.second<p2.second;
         return p1.first>p2.first;
     }
     
