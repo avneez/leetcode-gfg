98e3bf2b7b95581ae3c456ceb936437d7135f2b8
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-11-23 23:24:12 +0530

    Time: 75 ms (7.40%), Space: 20 MB (40.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,5 +1,28 @@
-// class Solution {
-// public:
+class Solution {
+public:
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> seen;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]!='.'){
+                    int k = (i/3)*3+j/3;
+                    string row = "row"+to_string(i)+board[i][j];
+                    string col = "col"+to_string(j)+board[i][j];
+                    string box = "box"+to_string(k)+board[i][j];
+                    if(seen.find(row)==seen.end() and seen.find(col)==seen.end() and seen.find(box)==seen.end()){
+                        seen.insert(row);
+                        seen.insert(col);
+                        seen.insert(box);
+                    }
+                    else return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+    
     
 //     bool isSafe(vector<vector<char>>& board , int temp, int row, int col){
 //         for(int i=0;i<board.size();i++)
@@ -42,21 +65,21 @@
 // };
 
 
-class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int row[9][9] = {0}, col[9][9] = {0}, sub[9][9] = {0};
+// class Solution {
+// public:
+//     bool isValidSudoku(vector<vector<char>>& board) {
+//         int row[9][9] = {0}, col[9][9] = {0}, sub[9][9] = {0};
         
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++)  {
-                if(board[i][j] != '.')  {
-                    int num = board[i][j] - '0' - 1, k = i/3*3 + j/3;
-                    if(row[i][num] || col[j][num] || sub[k][num])
-                        return false;
-                    row[i][num] = col[j][num] = sub[k][num] = 1;
-                }
-            }
-        }
-        return true;
-    }
-};
+//         for(int i = 0; i < 9; i++) {
+//             for(int j = 0; j < 9; j++)  {
+//                 if(board[i][j] != '.')  {
+//                     int num = board[i][j] - '1', k = i/3*3 + j/3;
+//                     if(row[i][num] || col[j][num] || sub[k][num])
+//                         return false;
+//                     row[i][num] = col[j][num] = sub[k][num] = 1;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
