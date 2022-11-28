a4b54e706b656b918cc7dbaca0bb4f2dfd09ac09
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-11-28 20:55:02 +0530

    Time: 515 ms (98.86%), Space: 187.8 MB (28.06%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,29 +1,59 @@
+// class Solution {
+// public:
+//     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+//         int n = matches.size();
+//         vector<vector<int>> v(2, vector<int>());
+//         // map<int,int> mpw;
+//         map<int,int> mpl;
+        
+//         for(int i =0; i<n; i++){
+//             int w = matches[i][0];
+//             int l = matches[i][1];
+//             // mpw[w]++;
+//             if(mpl.find(w)==mpl.end()){
+//                 mpl[w]=0;
+//             }
+//             mpl[l]++;
+//         }
+      
+//         for(auto it:mpl){
+//             if(it.second==1){
+//                 v[1].push_back(it.first);
+//             }
+//             else if(it.second==0){
+//                 v[0].push_back(it.first);
+//             }
+//         }
+//         return v;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        int n = matches.size();
-        vector<vector<int>> v(2, vector<int>());
-        // map<int,int> mpw;
-        map<int,int> mpl;
-        
-        for(int i =0; i<n; i++){
-            int w = matches[i][0];
-            int l = matches[i][1];
-            // mpw[w]++;
-            if(mpl.find(w)==mpl.end()){
-                mpl[w]=0;
+        vector<int> lossesCount(100001, -1);
+
+        for (auto& match : matches) {
+            int winner = match[0], loser = match[1];
+            if (lossesCount[winner] == -1) {
+                lossesCount[winner] = 0;
             }
-            mpl[l]++;
-        }
-      
-        for(auto it:mpl){
-            if(it.second==1){
-                v[1].push_back(it.first);
-            }
-            else if(it.second==0){
-                v[0].push_back(it.first);
+            if (lossesCount[loser] == -1) {
+                lossesCount[loser] = 1;
+            } else {
+                lossesCount[loser]++;
             }
         }
-        return v;
+
+        vector<vector<int>> answer(2, vector<int>());
+        for (int i = 1; i < 100001; ++i) {
+            if (lossesCount[i] == 0) {
+                answer[0].push_back(i);
+            } else if (lossesCount[i] == 1) {
+                answer[1].push_back(i);
+            }
+        }
+
+        return answer;
     }
 };
