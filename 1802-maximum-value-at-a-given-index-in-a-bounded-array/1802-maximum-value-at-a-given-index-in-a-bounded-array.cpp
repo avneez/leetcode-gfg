2fdc2d303f2174c817181e10fb968eda5fa13e03
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2023-06-11 05:11:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.04%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long calc(long n) {
+        return ((n * (n + 1)) / 2);
+    }
+    
+    long getSum(int totalLen, int val){
+        long totalSum = 0;
+        int decreasingLen = val;
+        
+        //[val, val - 1, val - 2, ... 1, 1, 1, 1]
+        if (totalLen >= decreasingLen){
+            long currSum = calc(val);
+            int remainingSum = totalLen - decreasingLen; //all 1's
+            totalSum = currSum + remainingSum;
+        }
+        //[val, val - 1, val - 2]
+        else {
+            long lastVal = decreasingLen - totalLen;
+            totalSum = calc(val) - calc(lastVal);
+        }
+        return totalSum; 
+    }
+
+    int maxValue(int n, int index, int maxSum) {
+        int low = 1, high = maxSum;
+        int leftCount = index, rightCount = n - index - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            long  leftSum = getSum(leftCount, mid - 1);
+            long  rightSum = getSum(rightCount, mid - 1);
+            long  totalSum = leftSum + mid + rightSum;
+            if (totalSum > maxSum)
+                high = mid - 1;
+            else { ans = mid; low = mid + 1; }
+        }
+        return ans; 
+    }
+};
